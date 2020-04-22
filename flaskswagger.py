b9523e53f454b45d5f3d28f05baf7de5cab9568e
paras <parasnandwani10@gmail.com>
2020-04-22 10:03:56 +0530

    Adding Flasgger Module to convert app to swagger and use api through UI

---
+++ flaskswagger.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 09:26:11 2020
+
+@author: parasnandwani10
+"""
+
+"""
+In this file we are generating UI with flasgger package
+"""
+
+import pickle
+from flask import Flask,request
+from flasgger import Swagger
+import numpy as np
+
+with open('rf.pkl','rb') as model_pkl:#reead binary raed as binary file not as usual file
+    model=pickle.load(model_pkl)#loading model pickel file
+    #perfectly train model ready to predictions
+    
+
+
+app=Flask(__name__)
+swagger=Swagger(app)# swaggerify flask application
+
+@app.route('/predict')
+def predict():
+    
+    """
+    Examole endpoint returning a prediction of iris
+    ---
+    parameters :
+    -name:s_length
+     in:query
+     type:number
+     required:true
+     
+    -name:s_width
+     in:query
+     type:number
+     required:true
+     
+    -name:s_length
+     in:query
+     type:number
+     required:true
+     
+    -name:s_width
+     in:query
+     type:number
+     required:true
+    """
+    s_length=request.args.get("s_length")
+    s_width=request.args.get("s_width")
+    p_length=request.args.get("p_length")
+    p_width=request.args.get("p_width")
+    predcition=model.predict(np.array([[s_length,s_width,p_length,p_width]]))
+    return str(predcition)
+    
+
+if __name__=='__main__':
+    app.run()
